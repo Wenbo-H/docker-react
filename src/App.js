68dec57c7f1531f187fe37b00c5529e855dcfddb
Wenbo-H <w27han@uwaterloo.ca>
2020-04-26 00:40:20 -0400

    Redeploy the app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey yo, how are you!
+          Hey yoo, how are you!
         </p>
         <a
           className="App-link"
